868cf38f7c06a2d9e3788dbf4b5f415683c7bbd4
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-08-22 08:54:30 +0300

    added styles to my javascript elements

--- api.js
+++ api.js
@@ -9,12 +9,20 @@ let products = document.getElementById('products')
 product.forEach(prolist => {
     let list = document.createElement('li')
     list.innerText = `${prolist.name}`
+    list.style.cssText = `
+    display:flex;
+    style: none;
+    padding:10px;
+    `
     products.appendChild(list)
 })
 })
 .catch(function(error){
  console.log('error',error);
 });
+document.getElementById("products").style.cssText = `
+  display: flex; 
+`;
 
 
 fetch('http://localhost:5000/products/fruits')
@@ -28,12 +36,20 @@ let fruits = document.getElementById('fruList')
 fruit.forEach(fru => {
     let fruitList = document.createElement('li')
     fruitList.innerText = `${fru.name}`
+    fruitList.style.cssText = `
+    display:flex;
+    style: none;
+    padding:10px;
+    `
     fruits.appendChild(fruitList)
 })
 })
 .catch(function(error){
  console.log('error',error);
 });
+document.getElementById("fruList").style.cssText = `
+  display: flex; 
+`;
 
 
 fetch('http://localhost:5000/products/vegetables')
@@ -47,46 +63,20 @@ let vegetables = document.getElementById('vegList')
 veggie.forEach(veg => {
     let veggieList = document.createElement('li')
     veggieList.innerText = `${veg.name}`
+    veggieList.style.cssText = `
+    display:flex;
+    style: none;
+    padding:10px;
+    `
     vegetables.appendChild(veggieList)
 })
 })
 .catch(function(error){
  console.log('error',error);
 });
+document.getElementById("vegList").style.cssText = `
+  display: flex; 
+`;
 
 
 
-// fetch('http://localhost:5000/products')
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(infos) {
-//  console.log(infos);
-
-// let myProduct = document.getElementById('fruList')
-// infos.forEach(y => {
-//     let productList = document.createElement('li')
-//     productList.innerText = `${y.name}`
-//     myProduct.appendChild(productList)
-// })
-// })
-// .catch(function(error){
-//  console.log('error',error);
-// });
-// fetch('http://localhost:5000/products')
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(infos) {
-//  console.log(infos);
-
-// let myProduct = document.getElementById('fruList')
-// infos.forEach(y => {
-//     let productList = document.createElement('li')
-//     productList.innerText = `${y.name}`
-//     myProduct.appendChild(productList)
-// })
-// })
-// .catch(function(error){
-//  console.log('error',error);
-// });
